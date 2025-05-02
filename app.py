e874742be1b12cf9d8a03498e50c85288832c796
Ryan T Tsikada <ryantsikada1601@gmail.com>
2025-05-02 15:15:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def get_recommendations(title, cosine_sim, df):
     music_indices = [i[0] for i in sim_scores]
     return df['title'].iloc[music_indices]
 
-st.title("🎧 Music Recommender System")
+st.title("🎧 RT Music Recommender System")
 df = load_data()
 cosine_sim = build_recommender(df)
 
